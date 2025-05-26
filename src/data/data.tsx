aa03324d6ea8737557353637175789e9f2299038
Christopher Joshua <65719392+chrisjosh1097@users.noreply.github.com>
2025-05-26 16:57:01 +1000

    First changes

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -69,18 +69,18 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Christopher Joshua`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a Jakarta based <strong className="text-stone-100">Data Scientist Graduate</strong>, from University of Sydney
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+         I'd describe myself as someone who's assertive, logical, and resourceful – I genuinely enjoy tackling 
+         different kinds of challenges and getting things done. 
+         From <strong className="text-stone-100">Programming, Modelling, Analysis or Project Management </strong>
+         I'm confident in my ability to pick up whatever new skills are needed{' '}
+    
       </p>
     </>
   ),
@@ -104,16 +104,16 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `As a Data Scientist graduate from the University of Sydney, 
+  my recent role as a Business Data Analyst at PT. Bank Central Asia has further 
+  honed my analytical thinking and problem-solving skills.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'Jakarta', Icon: MapIcon},
+    {label: 'Age', text: '28', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Indonesian', Icon: FlagIcon},
+    {label: 'Interests', text: 'Basketball, Gym, Guitar', Icon: SparklesIcon},
+    {label: 'Study', text: 'University of Sydney', Icon: AcademicCapIcon},
+    {label: 'Employment', text: ' - ', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -122,145 +122,141 @@ export const aboutData: About = {
  */
 export const skills: SkillGroup[] = [
   {
-    name: 'Spoken languages',
+    name: 'Machine Learning - Libraries',
     skills: [
       {
-        name: 'English',
-        level: 10,
-      },
-      {
-        name: 'French',
-        level: 4,
-      },
-      {
-        name: 'Spanish',
-        level: 3,
-      },
-    ],
-  },
-  {
-    name: 'Frontend development',
-    skills: [
-      {
-        name: 'React',
-        level: 9,
-      },
-      {
-        name: 'Typescript',
-        level: 7,
-      },
-      {
-        name: 'GraphQL',
-        level: 6,
-      },
-    ],
-  },
-  {
-    name: 'Backend development',
-    skills: [
-      {
-        name: 'Node.js',
+        name: 'Sci-kit-learn | Tensorflow | Keras | PyTorch',
         level: 8,
       },
       {
-        name: 'Rust',
-        level: 5,
+        name: 'Langchain | SpaCy',
+        level: 8,
       },
       {
-        name: 'Golang',
-        level: 4,
+        name: 'Numpy | Pandas',
+        level: 9,
       },
     ],
   },
   {
-    name: 'Mobile development',
+    name: 'Supporting Tools',
     skills: [
       {
-        name: 'React Native',
+        name: 'AWS',
+        level: 8,
+      },
+      {
+        name: 'Docker',
+        level: 8,
+      },
+      { 
+        name: 'Tableau',
+        level: 9,
+      },
+
+    ],
+  },
+  {
+    name: 'Database Management',
+    skills: [
+      {
+        name: 'PostgreSQL',
         level: 9,
       },
       {
-        name: 'Flutter',
-        level: 4,
+        name: 'MySQL',
+        level: 9,
       },
-      {
-        name: 'Swift',
-        level: 3,
-      },
+
     ],
   },
+  {
+    name: 'Frontend Development',
+    skills: [
+      {
+        name: 'ReactJS',
+        level: 7,
+      },
+      {
+        name: 'HTML',
+        level: 7,
+      },
+    ]
+  },
+
+
 ];
 
 /**
  * Portfolio section
  */
 export const portfolioItems: PortfolioItem[] = [
-  {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage1,
-  },
-  {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage2,
-  },
-  {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage3,
-  },
-  {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage4,
-  },
-  {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage5,
-  },
-  {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage7,
-  },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage11,
-  },
+  // {
+  //   title: 'Project title 1',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage1,
+  // },
+  // {
+  //   title: 'Project title 2',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage2,
+  // },
+  // {
+  //   title: 'Project title 3',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage3,
+  // },
+  // {
+  //   title: 'Project title 4',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage4,
+  // },
+  // {
+  //   title: 'Project title 5',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage5,
+  // },
+  // {
+  //   title: 'Project title 6',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage6,
+  // },
+  // {
+  //   title: 'Project title 7',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage7,
+  // },
+  // {
+  //   title: 'Project title 8',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage8,
+  // },
+  // {
+  //   title: 'Project title 9',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage9,
+  // },
+  // {
+  //   title: 'Project title 10',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage10,
+  // },
+  // {
+  //   title: 'Project title 11',
+  //   description: 'Give a short description of your project here.',
+  //   url: 'https://reactresume.com',
+  //   image: porfolioImage11,
+  // },
 ];
 
 /**
@@ -268,43 +264,49 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: '2025',
+    location: 'University of Sydney',
+    title: 'Masters in Data Science',
+    content: <p>Specialized in machine learning, AI engineering and visual analytics with hands-on experience in data pre-processing, model training, model testing, hyperparameter tuning, cloud computing and database management.</p>,
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: '2019',
+    location: 'Airlangga University',
+    title: 'Bachelor in Mathematics',
+    content: <p>Specialized in mathematical modelling and computer programming. Adept in areas such as network, operational research, statistics, business mathematics. </p>,
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: '2025',
+    location: 'Metasense - Sydney',
+    title: 'Machine Learning Engineer - Intern',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+      As part of the battery analytics team, I successfully developed State of Health (SoH) prediction models for both cell-level and module-level lithium-ion batteries. 
+      Doing ETL for multiple datasets, variety of machine learning techniques, I achieved high prediction accuracy with low MSE. 
+      This dual-level modeling approach enables more precise health diagnostics and a more versatile use case for our battery management systems. 
+      The models were validated and demonstrated strong generalization performance, making them suitable for real-world deployment in mobile phones and electric vehicles.
       </p>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: '2020 - 2024',
+    location: 'Bank Central Asia - Jakarta',
+    title: 'Senior Business Data Analyst',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Turning business and users’ needs into requirements. Built data pipelines with Python from SQL databases for easy to use and sustainable reporting. I've also developed 
+        impactful dashboards in Power BI to track quarterly project achievements that helped management track team and member productivity. I implemented test automation using 
+        Selenium for repetitive and complex workflow data. My experience includes working on credit applications, risk analysis, integrating with governmental services, adhering 
+        to regulatory requirements, and contributing to data warehouse development. I excel in presenting new ideas and product innovations to stakeholders.
       </p>
     ),
   },
 ];
+
+
 
 /**
  * Testimonial section
@@ -336,27 +338,27 @@ export const testimonial: TestimonialSection = {
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: 'You can e-mail or dm me on instagram.',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'chrisjosh1097@gmail.com',
+      href: 'mailto:chrisjosh1097@gmail.com',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'Jakarta, Indonesia',
+      href: 'https://maps.app.goo.gl/LqGX61kEeAMHCHEx5',
     },
     {
       type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: '@christojj',
+      href: 'https://www.instagram.com/christojj/',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'chrisjosh1097',
+      href: 'https://github.com/chrisjosh1097',
     },
   ],
 };
@@ -365,9 +367,9 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/chrisjosh1097'},
+  // {label: 'HuggingFace', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/christopher-j-06ba5b183/'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/christojj'},
+  // {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
 ];
