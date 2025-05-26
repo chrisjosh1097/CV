a0221e08e1e9f70f83d4d579683d2b75bc920497
Christopher Joshua <65719392+chrisjosh1097@users.noreply.github.com>
2025-05-26 20:21:56 +1000

    Updates

--- _next/static/QlJPWgaxrl1tsV_rcZtuN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/953-8b2617f48ff7ffd5.js","static/chunks/pages/index-77309fbde0f3ad08.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
